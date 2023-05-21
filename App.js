e77c4d8811d6c4625a44c53b12c6adb3b7100672
mahajanmuskaan <er.muskaan21@gmail.com>
2023-05-21 23:05:04 +0530

    React First Program

--- App.js
+++ App.js
@@ -7,7 +7,16 @@ console.log(element);
 
 // created root variable for the element having id = root in HTML, to which DOM manipulation will be done.
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
 
 // Added the new element in root.
 root.render(element);
+
+const element2 = React.createElement("h2", { className: "heading2", id: "demo2" }, "Wow!!"
+    , React.createElement("p", {}, "This is a paragraph element."));
+console.log(element2);
+    
+// created root2 variable for the element having id = left in HTML, to which DOM manipulation will be done.
+const root2 = ReactDOM.createRoot(document.getElementById("left"));
+
+// Added the new element in root2.
+root2.render(element2);
