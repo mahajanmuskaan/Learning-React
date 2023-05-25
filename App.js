498b24e72090f1becfb204cac8bf83ce2ea46a31
mahajanmuskaan <er.muskaan21@gmail.com>
2023-05-25 20:37:13 +0530

    Commit

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Using Pure React
+/*
+ * Using Pure React.
+ * The code demonstrates the creation and rendering of React elements to the DOM using React's createElement function
+ * and ReactDOM's createRoot and render methods.
+*/
 
 // Created h1 tag using React with message to display.
+
 const element = React.createElement(
     "h1",
     { className: "heading", id: "demo", key: "h1" },             // Introducing Key attribute in props for Concept- Reconciliation.
@@ -35,7 +40,7 @@ root2.render(element2);
 // Created new div element
 const divEle = React.createElement(
     "div",
-    {id:"div"},
+    { id: "div" },
     "This is a div element.",
     [element, element2]         // element1 and element2 are its children
 );
